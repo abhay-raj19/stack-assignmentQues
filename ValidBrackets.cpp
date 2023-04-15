8845461086d51d034b3384350e1d4fd57d3ae05f
abhay-raj19 <rajpootabhay423@gmail.com>
2023-04-15 15:24:17 +0530

    Adding minor changes

--- ValidBrackets.cpp
+++ ValidBrackets.cpp
@@ -1,9 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+
+bool isValid(string s) {
+    stack <char> st;
+    for(int i =0 ;i<s.length();i++){
+        char ch=s[i];
+        if (ch=='(' || ch=='{' || ch=='['){
+            st.push(ch);
+        }
+        else{
+            if(!st.empty()){
+            char top = st.top();
+            if((s[i]==')' && top =='(' ) || (s[i]=='}'&& top =='{' ) || (s[i]==']'&& top =='[' )){
+                st.pop();
+            } 
+            else{
+                return false;
+            }
+            }
+            else{
+                return false;
+            }
+        
+    }
+    
+}
+    if(st.empty()){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
    
    
 int main() {
-         
-       
+    string st1="{{{{{}}}}}";
+    string st2="{{[()()]}}";
+    string st3="{{()()())))}}";
+    cout<<isValid(st1)<<endl;    
+    cout<<isValid(st2)<<endl;    
+    cout<<isValid(st3)<<endl;           
     return 0;
 }
